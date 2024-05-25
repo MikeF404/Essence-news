f5ddec6093851af6dbf2877118ab8b6122678cca
mik-fedosenko <mik-fedosenko@yandex.ru>
2024-05-25 15:59:08 -0700

    Updated Activity Formatting

--- webpage-frontend/src/components/ArticleCard.tsx
+++ webpage-frontend/src/components/ArticleCard.tsx
@@ -1,40 +1,51 @@
-import { Button } from "@/components/ui/button"
+
 import {
     Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
 } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import React from 'react'
 
-export const ArticleCard = React.forwardRef(({ onClick, ...props }, ref) => (
-    <Card className="w-[650px] cursor-pointer" onClick={onClick} ref={ref} {...props}>
-        <CardHeader>
-            <CardTitle>Article Title</CardTitle>
-            <CardDescription>Description:</CardDescription>
-        </CardHeader>
-        <CardContent>
-            <form>
-                <div className="grid w-full items-center gap-4">
-                    <div className="flex flex-col space-y-1.5">
-                        <Label htmlFor="name">Name</Label>
-                        <Input id="name" placeholder="Name of your project" />
+import React from 'react'
+import {Dialog, DialogTrigger} from "@/components/ui/dialog.tsx";
+import ArticleDialogueContent from "@/components/ArticleDialogueContent.tsx";
+
+
+type ArticleCardProps = {
+    onClick?: React.MouseEventHandler<HTMLDivElement>;
+
+} & React.ComponentPropsWithoutRef<'div'>;
+
+export const ArticleCard = React.forwardRef<HTMLDivElement, ArticleCardProps>(
+    ({ onClick, ...props }, ref) => (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Card className="cursor-pointer" onClick={onClick} ref={ref} {...props}>
+                    <div className="flex justify-between pr-3 pl-3 items-center">
+                        <div className="">
+
+                                <p className="text-xl font-bold">
+                                    Article Title
+                                </p>
+                                <p className="text-foreground">
+                                    Publisher
+                                </p>
+
+
+                        </div>
+                        <div className="">
+                            <img
+                                className="mt-3 max-h-16 rounded-xl"
+                                src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4wwut?ver=b2fa"
+                                alt="article Image"
+                            />
+                        </div>
                     </div>
-                    <div className="flex flex-col space-y-1.5">
-                        <Label htmlFor="framework">Framework</Label>
-                    </div>
-                </div>
-            </form>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-            <Button variant="outline">Cancel</Button>
-            <Button>Deploy</Button>
-        </CardFooter>
-    </Card>
+                    <p className="p-2 pt-0">
+                        Short Description: labore et dolore magna aliqua Ut enim ad minim veniam, quis nostrud exercitation ullamco Laboris nisi ut aliquip ex a commodo consequat.
+                    </p>
+                   </Card>
+            </DialogTrigger>
+            <ArticleDialogueContent/>
+        </Dialog>
+
 ));
 
 ArticleCard.displayName = 'ArticleCard';
---
+++ webpage-frontend/src/components/Feed.tsx
@@ -0,0 +1,15 @@
+import {ArticleCard} from "@/components/ArticleCard.tsx";
+
+
+
+export default function Feed(){
+
+    return(
+        <div className="space-y-2">
+            <ArticleCard/>
+            <ArticleCard/>
+            <ArticleCard/>
+        </div>
+    );
+
+}
