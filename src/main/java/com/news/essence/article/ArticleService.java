ddd07fbd874bab8124c1e3f140f858ee398c72e1
mik-fedosenko <mik-fedosenko@yandex.ru>
2024-05-27 01:53:46 -0700

    The News API works: News are refreshed, and added to DB correctly

--- src/main/java/com/news/essence/article/ArticleService.java
+++ src/main/java/com/news/essence/article/ArticleService.java
@@ -86,6 +86,7 @@ public class ArticleService {
 
     @Transactional
     public void fetchAndStoreArticles() {
+        List<Article> articles;
         String url = getNewsApiUrl();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -93,78 +94,20 @@ public class ArticleService {
         Map<String, Object> payload = getPostPayload();
         HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(payload, headers);
 
-        /*
-        ResponseEntity<NewsApiResponse> response = restTemplate.exchange(
-                url, HttpMethod.POST, requestEntity, NewsApiResponse.class);
+        ResponseEntity<String> response = restTemplate.exchange(
+                url, HttpMethod.POST, requestEntity, String.class);
 
-        if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null && response.getBody().getResults() != null) {
-            List<Article> articles = response.getBody().getResults().stream()
-                    .filter(article -> !articleRepository.existsById(article.getUri()))
-                    .collect(Collectors.toList());
-         */
-        String jsonData = "{\n" +
-                "    \"articles\": {\n" +
-                "        \"results\": [\n" +
-                "            {\n" +
-                "                \"uri\": \"8145556983\",\n" +
-                "                \"lang\": \"eng\",\n" +
-                "                \"isDuplicate\": false,\n" +
-                "                \"date\": \"2024-05-25\",\n" +
-                "                \"time\": \"15:31:01\",\n" +
-                "                \"dateTime\": \"2024-05-25T15:31:01Z\",\n" +
-                "                \"dateTimePub\": \"2024-05-25T15:29:52Z\",\n" +
-                "                \"dataType\": \"news\",\n" +
-                "                \"sim\": 0,\n" +
-                "                \"url\": \"https://www.foxweather.com/weather-news/houston-power-outages-derecho-spotted-from-space\",\n" +
-                "                \"title\": \"Houston area's 1 million power outages after 100 mph derecho spotted from space\",\n" +
-                "                \"body\": \"HOUSTON -- The scars left after an intense derecho blasted the Houston area on May 16 are visible from space.\\n\\nThe line of thunderstorms tore through the city that evening, blistering Houston and its suburbs with wind gusts estimated up to 100 mph.\\n\\nWHAT IS A DERECHO?\\n\\nShattered glass littered downtown streets as the winds blew out dozens of windows from the city's skyscrapers while trees, power lines, and even 10 large electrical transmission towers were knocked over in the gusts.\\n\\nAbout 1 million electrical customers lost power in southeastern Texas that evening -- about 800,000 of them in the Houston metro area, officials said.\\n\\nThe blackouts were noticeable from satellites over 500 miles above Earth's surface. NASA's Suomi-NPP polar-orbiting satellite snapped before and after photos of the Houston metro area lit up at night, showing just how dark wide swaths of the city were in the storm's immediate aftermath.\\n\\nHOUSTON METRO ROCKED BY 100 MPH DERECHO THAT LEFT 7 DEAD AND OVER 1 MILLION WITHOUT POWER\\n\\nThousands of power crews were brought in to help restore electricity and a vast majority of outages have been repaired. CenterPoint Energy's current Houston-area outage page shows just over 1,600 customers still without power.\",\n" +
-                "                \"authors\": [\n" +
-                "                    {\n" +
-                "                        \"uri\": \"scott_sistek@foxweather.com\",\n" +
-                "                        \"name\": \"Scott Sistek\",\n" +
-                "                        \"type\": \"author\",\n" +
-                "                        \"isAgency\": false\n" +
-                "                    }\n" +
-                "                ],\n" +
-                "                \"categories\": [\n" +
-                "                    {\n" +
-                "                        \"uri\": \"dmoz/Business/Energy/Renewable\",\n" +
-                "                        \"label\": \"dmoz/Business/Energy/Renewable\",\n" +
-                "                        \"wgt\": 100\n" +
-                "                    },\n" +
-                "                    {\n" +
-                "                        \"uri\": \"dmoz/Science/Earth_Sciences/Atmospheric_Sciences\",\n" +
-                "                        \"label\": \"dmoz/Science/Earth Sciences/Atmospheric Sciences\",\n" +
-                "                        \"wgt\": 100\n" +
-                "                    },\n" +
-                "                    {\n" +
-                "                        \"uri\": \"dmoz/Science/Technology/Space\",\n" +
-                "                        \"label\": \"dmoz/Science/Technology/Space\",\n" +
-                "                        \"wgt\": 100\n" +
-                "                    },\n" +
-                "                    {\n" +
-                "                        \"uri\": \"dmoz/Business/Aerospace_and_Defense/Space\",\n" +
-                "                        \"label\": \"dmoz/Business/Aerospace and Defense/Space\",\n" +
-                "                        \"wgt\": 100\n" +
-                "                    }\n" +
-                "                ],\n" +
-                "                \"image\": \"https://static.foxweather.com/www.foxweather.com/content/uploads/2024/05/Houston_power_outages.gif\",\n" +
-                "                \"originalArticle\": null,\n" +
-                "                \"eventUri\": null,\n" +
-                "                \"sentiment\": -0.1137254901960785,\n" +
-                "                \"wgt\": 454347061,\n" +
-                "                \"relevance\": 26\n" +
-                "            }\n" +
-                "        ]\n" +
-                "    }\n" +
-                "}";
+        if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
 
-            List<Article> articles = parseJsonData(jsonData);
-            articleRepository.saveAll(articles);
-        //}
-        //logger.info("Response: {}", response.getStatusCode());
+            articles = parseJsonData(response.getBody());
+        } else {
+            logger.error("Failed to fetch articles. Status Code: {}, Response Body: {}", response.getStatusCode(), response.getBody());
+            return;  // Exit if the API call failed
+        }
         logger.info("Payload: {}", payload);
-        //logger.info("API Response: {}", response.getBody().toString());
+        logger.info("API Response: {}", response.getBody().toString());
+        logger.info("Saving articles: {}", articles);
+        articleRepository.saveAll(articles);
     }
 
     // A method to fill the DB with articles (can be triggered manually)
