aa1b6a189421083ef9754c544ad6a5781dfd61db
mik-fedosenko <mik-fedosenko@yandex.ru>
2024-06-08 23:45:55 -0700

    Added Theme Toggle (Dark/Light)

--- webpage-frontend/src/App.tsx
+++ webpage-frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import Feed from "@/components/Feed.tsx";
 import Header from "@/components/Header.tsx";
 import UserIdentifier from "@/components/UserIdentifier.tsx";
+import {ThemeProvider} from "@/components/ThemeProvider.tsx";
 function App() {
 
 
   return (
     <div className="p-20 flex justify-center bg-accent">
-        <UserIdentifier />
-        <Header />
-        <Feed/>
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+            <UserIdentifier />
+            <Header />
+            <Feed/>
+        </ThemeProvider>
+        
     </div>
   )
 }
--- webpage-frontend/src/components/Header.tsx
+++ webpage-frontend/src/components/Header.tsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react';
 import logo from "../assets/essence_logo.png";
+import {Button} from "@/components/ui/button.tsx";
+import {ThemeToggle} from "@/components/ThemeToggle.tsx";
+
 
 const Header = () => {
     const [showHeader, setShowHeader] = useState(true);
@@ -28,10 +31,25 @@ const Header = () => {
     }, [lastScrollY]);
 
     return (
-        <header className={`fixed top-0 w-full transition-transform duration-300 ${showHeader ? 'translate-y-0' : '-translate-y-full'}`}>
-            <div className="bg-accent-foreground text-background font-bold p-2 text-3xl flex items-center italic">
-                <img className="w-12 mr-3" src={logo} alt="logo"/>
-                <h1>Essence News</h1>
+        <header className={`fixed top-0 w-screen transition-transform duration-300 ${showHeader ? 'translate-y-0' : '-translate-y-full'}`}>
+            <div className="bg-accent-foreground text-accent  p-2 px-4 flex items-center justify-between">
+                <div className="flex font-bold italic text-3xl gap-3">
+                    <img className="w-12 " src={logo} alt="logo"/>
+                    <h1>Essence News</h1>
+                    <ThemeToggle/>
+                </div>
+                <div className="gap-2 flex text-foreground">
+
+                    <Button variant="outline" >
+                        Popular
+                    </Button>
+                    <Button className="" variant="outline" disabled >
+                        Personalized
+                    </Button>
+
+                </div>
+
+
             </div>
         </header>
     );
